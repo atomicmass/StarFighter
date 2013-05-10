d25569bf62c9c1332a19e83152a36815624a7d4f
Sean Coetzee <sean@epinion.co.za>
2013-05-10 10:54:03 +0200

    Various chnages

---
+++ StarFighter/Sprite.h
@@ -0,0 +1,13 @@
+//
+//  Sprite.h
+//  StarFighter
+//
+//  Created by Sean Coetzee on 2013/05/09.
+//
+//
+
+#import "CCSprite.h"
+
+@interface Sprite : CCSprite
+
+@end
---
+++ StarFighter/Sprite.m
@@ -0,0 +1,13 @@
+//
+//  Sprite.m
+//  StarFighter
+//
+//  Created by Sean Coetzee on 2013/05/09.
+//
+//
+
+#import "Sprite.h"
+
+@implementation Sprite
+
+@end
